af4e66bc6ce2e6c6f51ddab1f898883a9e65378a
JarCosta <joaoarcosta@tecnico.ulisboa.pt>
2023-10-18 21:07:51 +0100

    feat: final version of MEIC-T CU extractor

--- web scrape with python/ISTmeic-t.py
+++ web scrape with python/ISTmeic-t.py
@@ -1,6 +1,7 @@
 import json
 import requests
 from bs4 import BeautifulSoup
+import math
 
 def return_mid_and_after(full_string: str, left_limit: str, right_limit: str):
     after = full_string.split(left_limit, 1)[1]
@@ -26,6 +27,8 @@ def load_UCs():
     level = level.prettify()
     rest = level
 
+    with open(f"data/ISTmeic-t/UCs.html", "w") as outfile:
+        outfile.write(rest)
 
     subjects = {}
 
@@ -40,21 +43,25 @@ def load_UCs():
                 for i in subjects:
                     outfile.write(f"{i}:\n")
                     for j in subjects[i]:
-                        outfile.write(f"\t{j}\n")
+                        print(subjects[i][j])
+                        time = subjects[i][j][1].split(", ")[1].replace(" ", "").replace("Sem.", "S")
+                        outfile.write(f"\t{time} {j}\n")
                     outfile.write("\n")
             break
 
         if next_subject < next_CU:
             subject, rest = return_mid_and_after(rest, '<h4>\n  ', '\n </h4>')
-            subjects[subject] = []
+            subjects[subject] = {}
 
         else:
-            name, rest = return_mid_and_after(rest, 'style="font-size: 120%">\n     ', '\n    </a>')
+            href, rest = return_mid_and_after(rest, 'href="', '" style="font-')
+            name, rest = return_mid_and_after(rest, 'size: 120%">\n     ', '\n    </a>')
             time, rest = return_mid_and_after(rest, 'div>\n     ', '\n    </div>')
-            subjects[subject].append( name + " - " + time )
+            subjects[subject][name] = []
+            subjects[subject][name].extend([href, time])
 
 
-def filter_time(times: list = [], courses: list = []):
+def filter_time(times: list = [], courses: list = [], UCs: list = []):
 
     with open(f"data/ISTmeic-t/UCs.json", "r") as infile:
         data = json.load(infile)
@@ -63,19 +70,24 @@ def filter_time(times: list = [], courses: list = []):
 
     # Iterate through each key (course) and its values (course details)
     for course, details_list in data.items():
-        filtered_details = []
+        filtered_details = {}
+        
         
         if course not in courses and courses != []:
             continue
 
+        for i in UCs:
+            if i in details_list:
+                filtered_details[i] = details_list[i]
+
         # Iterate through each detail in the details_list
         for detail in details_list:
-            if times == []:
-                filtered_details.append(detail)
-            else:
+            # if times == []:
+            #     filtered_details[detail] = details_list[detail]
+            # else:
                 for time in times:
-                    if time in detail:
-                        filtered_details.append(detail)
+                    if time in details_list[detail]:
+                        filtered_details[detail] = details_list[detail]
 
         if filtered_details:
             filtered_data[course] = filtered_details
@@ -83,7 +95,7 @@ def filter_time(times: list = [], courses: list = []):
     for i in filtered_data:
         found = len(filtered_data[i])
         total = len(data[i])
-        filtered_data[i].append(f"{found}/{total}")
+        filtered_data[i]["found"] = f"{found}/{total}"
     
     # Convert the filtered_data to JSON format
     filtered_json = json.dumps(filtered_data, indent=4, ensure_ascii=False).encode('utf8').decode()
@@ -98,10 +110,21 @@ def filter_time(times: list = [], courses: list = []):
         for i in filtered_data:
             outfile.write(f"{i}:\n")
             for j in filtered_data[i]:
-                outfile.write(f"\t{j}\n")
+                if j != "found":
+                    outfile.write(f"\t{j}\n")
             outfile.write("\n")
 
 load_UCs()
 filter_time([], [])
-filter_time(["Ano 1, P 1", "Ano 1, P 2", "Ano 1, Sem. 1"], ["Engenharia e Ciência de Dados", "Robótica Inteligente"])
-filter_time(["Ano 1, P 1", "Ano 1, P 2", "Ano 1, Sem. 1"], [])
+
+times = ["Ano 1, P 4"]
+courses = ["Engenharia e Ciência de Dados", "Robótica Inteligente", "Inteligência Artificial", "Engenharia de Software"]
+UCs = ["Computação em Nuvem e Virtualização"]
+
+
+filter_time([], courses, [])
+filter_time(times, courses, [])
+filter_time([], [], UCs)
+filter_time(times, [], [])
+
+
