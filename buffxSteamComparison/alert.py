10f22e21d0b6c7357841fb2018937b0aa4cc1c25
JARCosta <1040costini@gmail.com>
2025-02-28 00:08:11 +0000

    fix: added alert fundtion changes for last commit

--- buffxSteamComparison/alert.py
+++ buffxSteamComparison/alert.py
@@ -7,17 +7,16 @@ import requests
 import telegramBot
 
 
-def alert(kill_thread:threading.Event):
+def alert(kill_thread:threading.Event, id:int):
     print("Alert Agent initialized")
     
     last_price = None
     while True:
-        id = 19909
         r = requests.get(f"https://api.buff.market/api/market/goods/sell_order?game=csgo&page_num=1&page_size=10&goods_id={id}&sort_by=default").json()
         if last_price != r["data"]["items"][0]["price"]:
             last_price = r["data"]["items"][0]["price"]
             print("new price")
-            telegramBot.sendMessage(str(last_price), notification=True)
+            telegramBot.sendMessage(f"{r['data']['goods_infos'][str(id)]['name']}: {last_price}$", notification=True)
         for _ in range(int(60)//5):
             time.sleep(5)
             if kill_thread.is_set():
