8ca7e7633e78cedf53fb77f39db2d62a18311189
JARCosta <1040costini@gmail.com>
2025-03-02 16:44:41 +0000

    fix: better alert messages with diff sign

--- buffxSteamComparison/alert.py
+++ buffxSteamComparison/alert.py
@@ -12,12 +12,23 @@ def alert(kill_thread:threading.Event, id:int):
     
     last_price = None
     while True:
-        r = requests.get(f"https://api.buff.market/api/market/goods/sell_order?game=csgo&page_num=1&page_size=10&goods_id={id}&sort_by=default").json() 
-        new_price = float(r["data"]["items"][0]["price"])
-        if last_price != new_price:
-            print("new price")
-            telegramBot.sendMessage(f"{r['data']['goods_infos'][str(id)]['name']}: {last_price}$ ({new_price - last_price if last_price != None else 'new'})", notification=True)
-            last_price = new_price
+
+        if last_price == None:
+            r = requests.get(f"https://api.buff.market/api/market/goods/sell_order?game=csgo&page_num=1&page_size=10&goods_id={id}&sort_by=default").json()
+            last_price = float(r["data"]["items"][0]["price"])
+            print("starting price")
+            telegramBot.sendMessage(f"{r['data']['goods_infos'][str(id)]['name']}: {last_price}$", notification=True)
+        else:
+            r = requests.get(f"https://api.buff.market/api/market/goods/sell_order?game=csgo&page_num=1&page_size=10&goods_id={id}&sort_by=default").json()
+            new_price = float(r["data"]["items"][0]["price"])
+            diff_price = new_price - last_price
+            
+            if diff_price != 0:
+                print("new price")
+                diff_sign = "+" if diff_price > 0 else "-"
+                diff_price = abs(diff_price)
+                telegramBot.sendMessage(f"{r['data']['goods_infos'][str(id)]['name']}: {new_price}$ ({diff_sign}{diff_price})", notification=True)
+                last_price = new_price
         for _ in range(int(60)//5):
             time.sleep(5)
             if kill_thread.is_set():
