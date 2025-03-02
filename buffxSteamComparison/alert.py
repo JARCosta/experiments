c95187ed8fb78ec04443d405ba9d99fd68af5e56
JARCosta <1040costini@gmail.com>
2025-03-02 16:23:43 +0000

    feat: price diff on price alerts

--- buffxSteamComparison/alert.py
+++ buffxSteamComparison/alert.py
@@ -12,11 +12,12 @@ def alert(kill_thread:threading.Event, id:int):
     
     last_price = None
     while True:
-        r = requests.get(f"https://api.buff.market/api/market/goods/sell_order?game=csgo&page_num=1&page_size=10&goods_id={id}&sort_by=default").json()
-        if last_price != r["data"]["items"][0]["price"]:
-            last_price = r["data"]["items"][0]["price"]
+        r = requests.get(f"https://api.buff.market/api/market/goods/sell_order?game=csgo&page_num=1&page_size=10&goods_id={id}&sort_by=default").json() 
+        new_price = float(r["data"]["items"][0]["price"])
+        if last_price != new_price:
             print("new price")
-            telegramBot.sendMessage(f"{r['data']['goods_infos'][str(id)]['name']}: {last_price}$", notification=True)
+            telegramBot.sendMessage(f"{r['data']['goods_infos'][str(id)]['name']}: {last_price}$ ({new_price - last_price if last_price != None else "new"})", notification=True)
+            last_price = new_price
         for _ in range(int(60)//5):
             time.sleep(5)
             if kill_thread.is_set():
